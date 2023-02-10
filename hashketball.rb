f0201c57192d7d95080b7351f8d12d730b3b739d
Bruno <brunochelimo@gmail.com>
2023-02-10 14:30:22 +0300

    done

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,107 @@ def game_hash
 end
 
 # Write code here
+
+def num_points_scored(player_name)
+  player_data = [alan_anderson, reggie_evans, brook_lopez, mason_plumlee, jason_terry, jeff_adrien, bismack_biyombo, desagna_diop, ben_gordon, kemba_walker].find { |player| player[:player_name] == player_name }
+  player_data[:points]
+end
+
+def shoe_size(player_name)
+  player_data = [alan_anderson, reggie_evans, brook_lopez, mason_plumlee, jason_terry, jeff_adrien, bismack_biyombo, desagna_diop, ben_gordon, kemba_walker].find { |player| player[:player_name] == player_name }
+  player_data[:shoe]
+end
+
+def team_colors(team_name)
+  teams = {
+    "Brooklyn Nets" => ["Black", "White"],
+    "Charlotte Hornets" => ["Turquoise", "Purple"]
+  }
+
+  teams[team_name]
+end
+
+def team_names
+  [
+    "Brooklyn Nets",
+    "Charlotte Hornets"
+  ]
+end
+
+def player_numbers(team_name)
+  players = [
+    {team: "Charlotte Hornets", number: 0, name: "Jeff Teague"},
+    {team: "Charlotte Hornets", number: 2, name: "Lamb"},
+    {team: "Charlotte Hornets", number: 4, name: "Miles Bridges"},
+    {team: "Charlotte Hornets", number: 8, name: "Bismack Biyombo"},
+    {team: "Charlotte Hornets", number: 33, name: "Cody Martin"},
+    {team: "Brooklyn Nets", number: 0, name: "Caris LeVert"},
+    {team: "Brooklyn Nets", number: 1, name: "Spencer Dinwiddie"},
+    {team: "Brooklyn Nets", number: 11, name: "Kyrie Irving"},
+    {team: "Brooklyn Nets", number: 30, name: "Taurean Prince"},
+    {team: "Brooklyn Nets", number: 31, name: "Rodions Kurucs"}
+  ]
+
+  numbers = []
+  players.each do |player|
+    if player[:team] == team_name
+      numbers << player[:number]
+    end
+  end
+
+  numbers
+end
+
+
+def player_stats(player_name)
+  case player_name
+  when "Jeff Adrien"
+    return jeff_adrien
+  when "Bismack Biyombo"
+    return bismack_biyombo
+  when "DeSagna Diop"
+    return desagna_diop
+  when "Ben Gordon"
+    return ben_gordon
+  when "Kemba Walker"
+    return kemba_walker
+  when "Alan Anderson"
+    return alan_anderson
+  when "Reggie Evans"
+    return reggie_evans
+  when "Brook Lopez"
+    return brook_lopez
+  when "Mason Plumlee"
+    return mason_plumlee
+  when "Jason Terry"
+    return jason_terry
+  else
+    return nil
+  end
+end
+
+
+
+def big_shoe_rebounds
+  players = [
+    { name: "Jeff Adrien", shoe_size: 12, rebounds: 11 },
+    { name: "Bismack Biyombo", shoe_size: 16, rebounds: 14 },
+    { name: "DeSagna Diop", shoe_size: 14, rebounds: 12 },
+    { name: "Ben Gordon", shoe_size: 15, rebounds: 5 },
+    { name: "Kemba Walker", shoe_size: 13, rebounds: 8 },
+    { name: "Alan Anderson", shoe_size: 14, rebounds: 10 },
+    { name: "Reggie Evans", shoe_size: 14, rebounds: 9 },
+    { name: "Brook Lopez", shoe_size: 17, rebounds: 11 },
+    { name: "Mason Plumlee", shoe_size: 16, rebounds: 13 },
+    { name: "Jason Terry", shoe_size: 15, rebounds: 2 }
+  ]
+
+  biggest_shoe_size = players.max_by { |player| player[:shoe_size] }[:shoe_size]
+  player_with_biggest_shoe = players.find { |player| player[:shoe_size] == biggest_shoe_size }
+  player_with_biggest_shoe[:rebounds]
+end
+
+
+
+
+
